3e400cea15be6395d40bca6ccd99d8af5847887c
Giuseppe Tribastone <giuseppet100@gmail.com>
2025-11-17 21:03:14 +0100

    sistemato viste e url

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,9 +4,11 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog/index.html'
+    paginate_by = 3
 
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
- 
- 
+
+    def get_queryset(self):
+        return Post.objects.filter(status=1)
